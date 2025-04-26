5a84ab88e7c100d219305a48d978e0abe9e9b3c0
csouthall1student <csouthall1@student.gsu.edu>
2025-04-26 17:35:40 -0400

    Update ml_dashboard.py

--- ml_dashboard.py
+++ ml_dashboard.py
@@ -173,26 +173,11 @@ with col[1]:
                  )
 
 
-with st.expander('About the Data', expanded=True):
-    st.write('''
-        - :red[**Data**]: [Kaggle Medical Insurance Premium](<https://www.kaggle.com/datasets/tejashvi14/medical-insurance-premium-prediction/data>).
-        - :orange[**Age**]: Age of customer.
-        - :orange[**Height**]: Height of customer.
-        - :orange[**Weight**]: Weight of customer.
-        - :orange[**Diabetes**]: Whether the person has abnormal blood sugar levels.
-        - :orange[**Blood Pressure Problems**]: Whether the person has abnormal blood pressure levels.
-        - :orange[**Any Transplants**]: Any major organ transplants.
-        - :orange[**Any Chronice Disease**]: Whether customer suffers from chronic ailments like asthama, etc.
-        - :orange[**Known Allergies**]: Whether the customer has any known allergies.
-        - :orange[**History of Cancer**]: Whether any blood relative of the customer has had any form of cancer.
-        - :orange[**Number of Major Surgeries**]: The number of major surgeries that the person has had.
-        - :green[**Premium Price**]: Target variable for prediction to create a model that predicts the yearly medical cover cost
-        ''')
+
  
 
-st.markdown("""### SHAP Analysis Commentary – Insurance Premium Prediction - HistGradientBoosting
- 
- This SHAP summary plot explains the impact of each feature on insurance premium predictions:
+with st.expander('SHAP Analysis Commentary – Insurance Premium Prediction - HistGradientBoosting', expanded=False):
+    st.markdown("""
  
  #### 1. `age`
  - Higher `age` values (pink dots on far right side of vertical 0 line) significantly increase predicted premiums.
@@ -216,6 +201,21 @@ st.markdown("""### SHAP Analysis Commentary – Insurance Premium Prediction - H
  
 st_shap(shap.plots.beeswarm(shap_values), height=500)
 
+with st.expander('About the Data', expanded=False):
+    st.write('''
+        - :red[**Data**]: [Kaggle Medical Insurance Premium](<https://www.kaggle.com/datasets/tejashvi14/medical-insurance-premium-prediction/data>).
+        - :orange[**Age**]: Age of customer.
+        - :orange[**Height**]: Height of customer.
+        - :orange[**Weight**]: Weight of customer.
+        - :orange[**Diabetes**]: Whether the person has abnormal blood sugar levels.
+        - :orange[**Blood Pressure Problems**]: Whether the person has abnormal blood pressure levels.
+        - :orange[**Any Transplants**]: Any major organ transplants.
+        - :orange[**Any Chronice Disease**]: Whether customer suffers from chronic ailments like asthama, etc.
+        - :orange[**Known Allergies**]: Whether the customer has any known allergies.
+        - :orange[**History of Cancer**]: Whether any blood relative of the customer has had any form of cancer.
+        - :orange[**Number of Major Surgeries**]: The number of major surgeries that the person has had.
+        - :green[**Premium Price**]: Target variable for prediction to create a model that predicts the yearly medical cover cost
+        ''')
 # In[ ]:
 
 
